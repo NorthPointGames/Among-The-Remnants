65fe07b47ab07f88f4edbed6ac4d8a8645e82fce
Arct3ch <113224776+Arct3ch@users.noreply.github.com>
2023-08-05 14:28:35 -0800

    AI Progress

---
+++ Source/InventoryComponents/EQ_Roam.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQuery.h"
+#include "EQ_Roam.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYCOMPONENTS_API UEQ_Roam : public UEnvQuery
+{
+	GENERATED_BODY()
+	
+};
